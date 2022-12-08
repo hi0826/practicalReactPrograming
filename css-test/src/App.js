5e863ee6121c18a5879aadc345b597c557db04c8
최홍일 <hongil.choi@nts-corp.com>
2022-12-08 18:04:57 +0900

    classnames 사용

--- css-test/src/App.js
+++ css-test/src/App.js
@@ -2,17 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './Button1';
 import Button2 from './Button2';
-import Box from './Box1';
+import Box1 from './Box1';
+import Box2 from './Box2';
 
 function App() {
   return (
     <div>
       <Button size="big" />
       <Button size="small" />
-      <Box size="big" />
-      <Box size="small" />
+      <Box1 size="big" />
+      <Box1 size="small" />
       <Button2 size="big" />
       <Button2 size="small" />
+      <Box2 size="big" />
+      <Box2 size="small" />
     </div>
   );
 }
